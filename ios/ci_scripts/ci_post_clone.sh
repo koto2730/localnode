0be6d396f54349e26f0c3d3ae1def74feed55e81
koto2730 <koto2730oss@gmail.com>
2025-12-30 11:21:09 +0900

    fix: adjust for build_1.0.0

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-# 1. Flutter SDKのクローン（安定版）
+# エラーが発生したら即座に停止するように設定
+set -e
+
+# 1. Flutter SDKのクローン
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
 
-# 2. プレフライントチェックと依存関係の解決
-flutter doctor
+# 2. プロジェクトルートに戻ってpub get
+# ci_scripts の中から実行されるので、cd .. でルートに戻る
+cd ..
 flutter pub get
 
-# 3. CocoaPodsのインストール
-HOMEBREW_NO_AUTO_UPDATE=1 brew install cocoapods
-cd ..
+# 3. CocoaPodsのインストール（Xcode Cloudはbrewが遅い場合があるので、gemを検討）
+# すでに入っている場合はスキップ
+if ! command -v pod &> /dev/null; then
+    sudo gem install cocoapods
+fi
+
+# 4. Podのインストール
+cd ios
 pod install
